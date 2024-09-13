f8e829b555f8f7c052703a4b5b32e3d4559232ee
CGM <mrmouse.guru@gmail.com>
2024-09-13 15:34:23 +0700

    updated Basic Source code

---
+++ src/AddUIConsoleInput.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class AddUIConsoleInput {
+
+    //fields
+    PrintWriter stdOut = null;
+    BufferedReader stdIn = null;
+    AddUseCaseControl addUseCaseControl = null;
+
+    //methods
+    AddUIConsoleInput(){
+        stdOut = new PrintWriter(
+            new BufferedWriter(
+                new OutputStreamWriter(System.out)
+            ), true
+        );
+
+        stdIn = new BufferedReader(
+            new InputStreamReader(System.in));
+
+    }
+
+    public void input() throws IOException{
+        stdOut.print("Input number1:");
+        stdOut.flush();
+        String strNumber1 = stdIn.readLine();
+    }
+
+}
---
+++ src/AppSum.java
@@ -0,0 +1,7 @@
+public class AppSum {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Addition Application");
+        AddUIConsoleInput addUIConsoleInput = new AddUIConsoleInput();
+        addUIConsoleInput.input();
+    }
+}
